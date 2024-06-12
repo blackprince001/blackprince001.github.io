b795371a6b5d9f802c189ba92c13ea6ed775f999
blackprince001 <appiahboaduprince@gmail.com>
2024-06-12 15:12:48 +0000

    hmmm

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/blackprince001.github.io",
+  output: "export",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
