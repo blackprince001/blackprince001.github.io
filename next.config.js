96397ca2b2f34f660861c457f68e039381c96915
blackprince001 <appiahboaduprince@gmail.com>
2024-06-17 12:59:42 +0000

    add bottle-tutorial glb

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/blackprince001.github.io",
-  output: "export",
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
