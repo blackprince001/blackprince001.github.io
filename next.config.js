c734e97d11381a64c496a4b11af7866fb0e28293
blackprince001 <appiahboaduprince@gmail.com>
2024-06-17 13:14:41 +0000

    fix deployment issue

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/blackprince001.github.io",
+  output: "export",
+  reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+};
+
+module.exports = nextConfig;
