24b4657d630a207a10f15124a3a404c53bad1462
blackprince001 <appiahboaduprince@gmail.com>
2024-11-14 17:17:50 +0000

    add drone

--- src/app/(main)/components/hero.tsx
+++ src/app/(main)/components/hero.tsx
@@ -1,5 +1,6 @@
 import ProudGhanaian from "@/components/ui/pg";
 import Link from "next/link";
+import Image from "next/image";
 
 function HeroSection() {
   return (
@@ -25,6 +26,7 @@ function HeroSection() {
         I had my undergraduate studies at <Link className="hover:underline" href="https://www.knust.edu.gh">Kwame Nkrumah University of Science and Technology.</Link>
         </p>
         <br />
+          <Image src='https://blackprince001.github.io/images/drone.png' alt={"Drone"} width={500} height={600}/>
         <p>
         My passion lies in creating interactive software and systems that fosters idea sharing and personal expression. As a generalist, I am deeply invested in both technical and non-technical fields: hoping to bridge the gap between them. I believe that these fundamental aspects are crucial for developing meaningful products and research.
         </p>
--- src/app/(posts)/blog/layout.tsx
+++ src/app/(posts)/blog/layout.tsx
@@ -1,35 +1,34 @@
 import { Metadata } from "next";
 import "katex/dist/katex.min.css";
-import Comments from "@/app/(main)/components/comments";
 
 export const metadata: Metadata = {
-    title: "blackprince | Nebula",
+  title: "blackprince | Nebula",
+  description: "Engineering Journal",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+  openGraph: {
+    title: "blackprince",
     description: "Engineering Journal",
-    icons: {
-      icon: "/favicon.ico",
-      shortcut: "/favicon.ico",
-      apple: "/favicon.ico",
-    },
-    openGraph: {
-      title: "blackprince",
-      description: "Engineering Journal",
-      url: "https://blackprince001.github.io",
-      siteName: "blackprince001.github.io",
-      images: [
-        {
-          url: "https://blackprince001.github.io/images/blog.png",
-          width: 800,
-          height: 600,
-        },
-        {
-          url: "https://blackprince001.github.io/images/blog.png",
-          width: 1800,
-          height: 1600,
-          alt: "blackprince/blog-post",
-        },
-      ],
-    },
-  };
+    url: "https://blackprince001.github.io",
+    siteName: "blackprince001.github.io",
+    images: [
+      {
+        url: "https://blackprince001.github.io/images/blog.png",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://blackprince001.github.io/images/blog.png",
+        width: 1800,
+        height: 1600,
+        alt: "blackprince/blog-post",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
